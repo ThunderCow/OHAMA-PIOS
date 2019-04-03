393fbfe049277177974140dee1c13e3d99805a2d
Ole Morten <31931528+ThunderCow@users.noreply.github.com>
2019-04-03 12:35:39 +0000

    Update exportCsv.py

--- testcode/OhamaPy/exportCsv.py
+++ testcode/OhamaPy/exportCsv.py
@@ -10,11 +10,11 @@ class CsvExport:
     def __init__(self):
         with open('dataFile.csv', 'w') as data_file:
             data_writer = writer(data_file)
-            data_writer.writerow(['Temp,Pressure,Humidity,Temp_from_pressure,Temp_from_humidity'])
+            data_writer.writerow(['Date_Time,Temp,Pressure,Humidity,Temp_from_pressure,Temp_from_humidity'])
             data_file.close()
 
-    def writeData(self,input1, input2, input3, input4, input5):
+    def writeData(self,dt_log, input1, input2, input3, input4, input5):
         with open('dataFile.csv', 'a') as data_file:
             data_writer = writer(data_file)
-            data_writer.writerow([str(input1)+','+str(input2)+','+str(input3)+','+str(input4)+','+str(input5)])
+            data_writer.writerow([str(dt_log)+','+str(input1)+','+str(input2)+','+str(input3)+','+str(input4)+','+str(input5)])
             data_file.close()
